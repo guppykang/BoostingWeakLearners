58ee3739a3e0138cd80a3c70a3db3c78d80b4c59
Joshua Kang <guppykang@gmail.com>
2019-06-04 14:20:19 -0700

    inital commit

---
+++ pa5.py
@@ -0,0 +1,16 @@
+import collections
+import math
+import numpy
+from random import randint
+
+def loadData(fileName, features=[], labels=[]):
+    fileOpen = open(fileName, "r")
+    lines = fileOpen.readlines()
+    fileOpen.close()
+    for unFormattedLine in lines:
+        line = unFormattedLine.split()
+        currentLine = []
+        for feature in line[:-1]:
+            currentLine.append(float(feature))
+        features.append(currentLine)
+        labels.append(line[len(line)-1])
