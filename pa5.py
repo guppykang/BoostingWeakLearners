2bbf27a98c6935df7cb903e26fb7de3e2a2590d7
Joshua Kang <guppykang@gmail.com>
2019-06-04 14:33:31 -0700

    started finding classifier

--- pa5.py
+++ pa5.py
@@ -1,7 +1,6 @@
 import collections
 import math
 import numpy
-from random import randint
 
 def loadData(fileName, features=[], labels=[]):
     fileOpen = open(fileName, "r")
@@ -14,3 +13,37 @@ def loadData(fileName, features=[], labels=[]):
             currentLine.append(float(feature))
         features.append(currentLine)
         labels.append(line[len(line)-1])
+
+def loadDict(fileName, totalWords=[]):
+    fileOpen = open(fileName, "r")
+    lines = fileOpen.readlines()
+    fileOpen.close()
+    for unFormattedLine in lines:
+        line = unFormattedLine.split()
+        totalWords.append(line[0])
+        
+
+def getBestClassifier(words, emails, labels, w): 
+    classifiers = []
+
+    for word in words: 
+        errorPositive = 0
+        errorNegative = 0
+
+        wordIndex = words.index(word)
+
+        for email in emails:
+            if email[wordIndex] == 1 and labels[emails.inedx(email)] == -1:
+                #increment the error Positive 
+            if email[wordIndex] == 1 and labels[emails.inedx(email)] == 1:
+                #increment the error negative
+
+
+
+trainingFeatures = []
+trainingLabels = []
+words = []
+
+loadData('pa5train.txt', trainingFeatures, trainingLabels)
+loadDict('pa5dictionary.txt', words)
+print(words)
